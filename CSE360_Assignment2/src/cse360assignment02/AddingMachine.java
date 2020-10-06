b17589702e7840b0c4b9d09d529e1e49962736ba
TKaing.22 <tckaing@asu.edu>
2020-10-05 21:53:46 -0700

    Initial commit of Assignment2

---
+++ CSE360_Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,60 @@
+package cse360assignment02;
+
+/**
+* The AddingMachine class implements various methods to calculate simple 
+* arithmetic operations that are stored and printed upon request. The class 
+* contains methods getTotal(), add(), subtract(), toString(), and clear().
+* 
+* @author Tina Kaing
+* @version 1.0
+* @since   2020-10-05 
+*/
+
+
+public class AddingMachine {
+	  private int total;
+	  
+	  
+	  /**
+	   * The AddingMachine constructor instantiates total to 0
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * The getTotal method returns the total value of the program
+	   * @return stored total of program
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * The add method adds the passed value to the total
+	   * @param value summed with total
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * The subtract method subtracts the passed value from the total
+	   * @return value subtracted from total
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * The toString method records all arithmetic operations in AddingMachine
+	   * @return arithmetic operations record in string format
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * The clear method resets the current value of total to 0
+	   */
+	  public void clear() {
+	  }
+	}
