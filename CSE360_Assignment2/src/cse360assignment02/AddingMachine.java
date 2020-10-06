b62b5ac3985ffdc683be44bb60a2e300fb3e8765
TKaing.22 <TKaing.22@localhost>
2020-10-05 22:23:46 -0700

    Second commit of Assignment2

--- CSE360_Assignment2/src/cse360assignment02/AddingMachine.java
+++ CSE360_Assignment2/src/cse360assignment02/AddingMachine.java
@@ -6,20 +6,21 @@ package cse360assignment02;
 * contains methods getTotal(), add(), subtract(), toString(), and clear().
 * 
 * @author Tina Kaing
-* @version 1.0
+* @version 2.0
 * @since   2020-10-05 
 */
 
 
 public class AddingMachine {
 	  private int total;
-	  
+	  private String history;
 	  
 	  /**
-	   * The AddingMachine constructor instantiates total to 0
+	   * The AddingMachine constructor instantiates total to 0 and history to output "0"
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    history = "0";
 	  }
 	  
 	  /**
@@ -31,30 +32,38 @@ public class AddingMachine {
 	  }
 	  
 	  /**
-	   * The add method adds the passed value to the total
+	   * The add method adds the passed value to the total and 
+	   * updates the history string with any arithmetic modifications
 	   * @param value summed with total
 	   */
 	  public void add (int value) {
+		  total = total + value;
+		  history = history + " + " + value;
 	  }
 
 	  /**
-	   * The subtract method subtracts the passed value from the total
+	   * The subtract method subtracts the passed value from the total and 
+	   * updates the history string with any arithmetic modifications
 	   * @return value subtracted from total
 	   */
 	  public void subtract (int value) {
+		  total = total - value;
+		  history = history + " - " + value;
 	  }
 
 	  /**
 	   * The toString method records all arithmetic operations in AddingMachine
-	   * @return arithmetic operations record in string format
+	   * @return history of arithmetic operations in string format
 	   */
 	  public String toString () {
-	    return "";
+	    return history;
 	  }
 
 	  /**
-	   * The clear method resets the current value of total to 0
+	   * The clear method resets the current value of total to 0 and history to "0"
 	   */
 	  public void clear() {
+		  total = 0;
+		  history = "0";
 	  }
 	}
